2c6c8d109a24c71688e094cf5a8fd1fbbc05aad3
Issouf DIarrassouba <112406158+Issouf-Diarrassouba@users.noreply.github.com>
2025-04-01 15:04:11 -0400

    Updated configure.swift

--- Backend/Sources/App/configure.swift
+++ Backend/Sources/App/configure.swift
@@ -3,17 +3,15 @@ import FluentSQLiteDriver
 import Vapor
 
 public func configure(_ app: Application) async throws {
+    // This is what exposes your server to the internet
+    app.http.server.configuration.hostname = "0.0.0.0"
+    
     app.databases.use(DatabaseConfigurationFactory.sqlite(.file("db.sqlite")), as: .sqlite)
     app.middleware.use(CORSMiddleware(configuration: .default()))
 
     app.migrations.add(CreateUser())
-//    app.migrations.add(CreateProperty())
-//    Needed to communicate with the frontend functionality
-    // Important for creating the user preference
     app.migrations.add(CreateUserPreference())
 
-
     try await app.autoMigrate()
-
     try routes(app)
 }
